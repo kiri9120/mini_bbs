b90f085ed5f834c096332383f45ae5da92cf7749
Masato Kiryu <kiri9120@gmail.com>
2021-01-07 22:48:19 +0900

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,16 @@
 <?php
 try {
-    $db = new PDO('mysql:dbname=mini_bbs;port=8889;host=localhost;charset=utf8', 'root', 'root');
+    // $db = new PDO('mysql:dbname=mini_bbs;port=8889;host=localhost;charset=utf8', 'root', 'root');
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $db_name = substr($url["path"], 1);
+    $db_host = $url["host"];
+    $user = $url["user"];
+    $password = $url["pass"];
+
+    $dsn = "mysql:dbname=".$db_name.";host=".$db_host;
+
+    $pdo=new PDO($dsn,$user,$password,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 } catch(PDOException $e) {
     print('DB接続エラー：' . $e->getMessage());
 }
