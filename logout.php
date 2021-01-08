89b321e6fef1258818af7bfe918ca6c261a33c27
Masato Kiryu <kiri9120@gmail.com>
2021-01-08 15:15:32 +0900

    ini_set

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 session_start();
 
 $_SESSION = array(); //空の配列で上書き
-if(ini_set('sesson.use_cookies')) {
+if(ini_set('sesson.use_cookies', '1')) {
     $params = session_get_cookie_params();
     setcookie(session_name() . '', time() - 42000,
     $params['path'], $params['domain'], $params['secure'], $params['httponly']);
